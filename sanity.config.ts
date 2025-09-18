b6604fd5f6de249e321b9118ee4047fbfa06ba58
Freekyn <vxamsi@outlook.com>
2025-09-18 13:11:52 +0530

    sanity.config changed

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,27 +1,13 @@
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `/app/admin/[[...tool]]/page.tsx` route
- */
+import type { StructureResolver } from "sanity/desk";
 
-import { visionTool } from "@sanity/vision";
-import { defineConfig } from "sanity";
-// The correct import for the main studio interface is `deskTool` from `sanity/desk`.
-import { deskTool } from "sanity/desk";
-
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import { apiVersion, dataset, projectId } from "./src/sanity/env";
-import { schema } from "./src/sanity/schema";
-
-export default defineConfig({
-  basePath: "/admin",
-  projectId,
-  dataset,
-  // Add and edit the content schema in the './src/sanity/schema' folder
-  schema,
-  plugins: [
-    // Use the imported deskTool
-    deskTool(),
-    // Vision is a tool that lets you query your content with GROQ in the studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({ defaultApiVersion: apiVersion }),
-  ],
-});
+// This is a basic structure resolver. You can customize it later to organize your documents in the Studio.
+// https://www.sanity.io/docs/structure-builder-cheat-sheet
+export const structure: StructureResolver = (S) =>
+  S.list()
+    .id("root")
+    .title("Content")
+    .items([
+      // Add your document types here. For example:
+      S.documentTypeListItem("milestone").title("Milestones"),
+      S.documentTypeListItem("galleryImage").title("Gallery Images"),
+    ]);
